9192c5e72a8e914eb3c76ee227a11b9c4248f840
Jeremy <jeremyjosecastellanos@gmail.com>
2026-05-06 21:10:54 -0600

    - definicion de variables de estado en app y custominput - implementacion de comportamiento condicionado para componentes (mostrar/ocultar botones, texto) - implementacion de tipos de teclado personalizados - errores de validacion condicionados -  iconos condicionados - aplicacion de multiples estilos a un solo componente - estilos condicionados

--- MiPrimeraApp/App.tsx
+++ MiPrimeraApp/App.tsx
@@ -2,19 +2,42 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { CustomButton } from "./src/components/CustomButton";
 import CustomInput from "./src/components/CustomInput";
+import { useState } from "react";
 
 export default function App() {
+  // Definicion de una variable de estado en Ract
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+
   return (
     <View style={styles.container}>
       <Text>Hello Jeremy</Text>
       <StatusBar style="auto" />
       <CustomInput
+        type={"text"}
+        placeholder={"Ingresa tu nombre"}
+        value={name}
+        onChange={setName}
+      />
+      <CustomInput
         type={"number"}
-        placeholder={""}
-        value={""}
-        onChange={function (text: string): void {
-          throw new Error("Function not implemented.");
-        }}
+        placeholder={"Ingresa tu numero"}
+        value={number}
+        onChange={setNumber}
+      />
+      <CustomInput
+        type={"email"}
+        placeholder={"Ingresa tu email"}
+        value={email}
+        onChange={setEmail}
+      />
+      <CustomInput
+        type={"password"}
+        placeholder={"Ingresa tu contraseña"}
+        value={password}
+        onChange={setPassword}
       />
       <CustomButton
         title={"App"}
--- MiPrimeraApp/src/components/CustomInput.tsx
+++ MiPrimeraApp/src/components/CustomInput.tsx
@@ -1,30 +1,107 @@
-import { Ionicons } from "@expo/vector-icons";
-import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import { MaterialIcons, Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import {
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  KeyboardTypeOptions,
+} from "react-native";
 
 type Props = {
-  type: "text" | "email" | "password" | "number";
+  type?: "text" | "email" | "password" | "number";
   placeholder: string;
   value: string;
   onChange: (text: string) => void;
 };
 
 export default function CustomInput({
-  type,
+  type = "text",
   placeholder,
   value,
   onChange,
 }: Props) {
+  const [isSecureText, setIsSecureText] = useState(type === "password");
+  const isPasswordField = type === "password";
+  const icon: typeof MaterialIcons["name"] | undefined =
+    type === "email"
+      ? "alternate-email"
+      : type === "password"
+        ? "lock"
+        : undefined;
+
+  const keyboardType: KeyboardTypeOptions =
+    type === "email"
+      ? "email-address"
+      : type === "number"
+        ? "phone-pad"
+        : "default";
+
+  const getError = () => {
+    if (type === "email" && !value.includes("@")) return "Correo invalido";
+    if (type === "password" && value.length < 4)
+      return "La contraseña es muy debil";
+    if ((type === "number" && value.length! < 8) || value.includes("-"))
+      return "Numero de telefono invalido";
+  };
+
+  const error = getError();
   return (
-    <View>
-      <TextInput
-        placeholder={placeholder}
-        value={value}
-        onChangeText={onChange}
-      />
-      <TouchableOpacity>
-        <Text>Icon Button</Text>
-        <Ionicons name={"eye"} size={14} />
-      </TouchableOpacity>
+    // Wrapper
+    <View style={styles.wrapper}>
+      {/* Container */}
+      <View style={[styles.inputContainer, error && styles.inputError]}>
+        <MaterialIcons name={icon as any} size={22} color={"#000"} />
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          value={value}
+          onChangeText={onChange}
+          secureTextEntry={isSecureText}
+          keyboardType={keyboardType}
+        />
+        {isPasswordField && (
+          <TouchableOpacity
+            onPress={() => {
+              setIsSecureText(!isSecureText);
+            }}
+          >
+            <Ionicons name={isSecureText ? "eye" : "eye-off"} size={22} />
+          </TouchableOpacity>
+        )}
+      </View>
+      {error && <Text style={styles.inputError}>{error}</Text>}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginBottom: 10,
+  },
+  inputContainer: {
+    // Distribucion de componentes
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+
+    // Estilizacion de input
+    borderColor: "gray",
+    borderWidth: 1,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+    paddingLeft: 20,
+  },
+
+  input: {
+    width: "80%",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+
+  inputError: {
+    color: "red",
+    borderColor: "red",
+  },
+});
